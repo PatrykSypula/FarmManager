cff66301141c837b5e71947f62b0c6bb8b7b7ce3
Patryk <patryk.sypula@gmail.com>
2025-10-16 20:15:04 +0200

    last changes

--- FarmManager.Model/Model/Buy.cs
+++ FarmManager.Model/Model/Buy.cs
@@ -8,7 +8,7 @@ public class Buy : BaseEntity, IDescribable
     private decimal _quantity;
     public decimal Quantity { get => _quantity; set => _quantity = Math.Round(value, 2); }
     private decimal _remainingQuantity;
-    public decimal RemainingQuantity { get => _remainingQuantity; set => _quantity = Math.Round(value, 2); }
+    public decimal RemainingQuantity { get => _remainingQuantity; set => _remainingQuantity = Math.Round(value, 2); }
     public int VendorId { get; set; }
     public Vendor Vendor { get; set; } = null!;
     public int FertilizerId { get; set; }
